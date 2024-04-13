c4d74da7a365f7aacf9782a31ade6ccb7c405f77
vakharwalad23 <vakharwala.d@gmail.com>
2024-04-13 20:44:58 +0530

    init

---
+++ apps/server/src/index.ts
@@ -0,0 +1,20 @@
+import http from 'http'
+import Socket from './services/socket';
+
+async function startServer() {
+    const socket = new Socket();
+    
+    const server = http.createServer();
+    const PORT = process.env.PORT ?? 8000;
+
+    //Server attached to socket.io
+    socket.io.attach(server);
+
+    socket.intitListeners();
+
+  server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+  })
+}
+
+startServer();
---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,30 @@
+import { Server } from 'socket.io'
+
+class Socket {
+    private _io: Server;
+    constructor() {
+        this._io = new Server();
+        console.log('Socket server initialized');
+    }
+
+    public intitListeners() {
+        const io = this.io;
+        console.log('Socket server listening for connections');
+        io.on('connect', (socket) => {
+            console.log('New Client connected', socket.id);
+
+            socket.on('event:message', async( { message }: { message: string }) => {
+                console.log('New Message received', message);
+            })
+        })
+        io.on('disconnect', (socket) => {
+            console.log('Client disconnected', socket.id);
+        })
+    }
+
+    get io() {
+        return this._io;
+    }
+}
+
+export default Socket;
